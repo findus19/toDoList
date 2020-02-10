3e4c24c4559e3a640f0930a6c7e85ad7db42549f
findus19 <ivanexs@mail.ru>
2020-02-10 21:58:05 +0300

    day13 try

--- js/script.js
+++ js/script.js
@@ -5,59 +5,76 @@ let input = document.querySelector('input.header-input'),
     addNewToDo = document.getElementById('add'),
     ulToDo = document.querySelector('ul.todo'),
     ulToDoComplete = document.querySelector('ul.todo-completed');
-    console.log(ulToDoComplete);
 
-let local = {
-    textLi1: '',
-    divBtn1: '',
-    btnRemove1: '',
-    btnComplete1: '',
-    ulToDo1: ''
-}
 
-ulToDo.textContent = localStorage.getItem('items');
+
+/* function loadToDoComplete(){
+    const dataComplete = localStorage.getItem('itemsComplete');
+    if(dataComplete){
+        ulToDoComplete.innerHTML = dataComplete;
+    }
+    const btnRemoveLoad = document.querySelectorAll('button.todo-remove');
+    for(const button of btnRemoveLoad){
+        removeToDo(button);
+    } 
+};
+
+
+loadToDoComplete() */
+
+
 
 const addToDo = function(){
+
     event.preventDefault();
     const textLi = document.createElement('li');
     textLi.classList.add('todo-item');
     const newToDo = input.value;
     textLi.append(newToDo);
-    local.textLi1 = textLi;
 
     const divBtn = document.createElement('div');
     divBtn.classList.add('todo-buttons');
-    local.divBtn1 = divBtn;
 
     const btnRemove = document.createElement('button');
     btnRemove.classList.add('todo-remove');
-    local.btnRemove1 =btnRemove;
 
     const btnComplete = document.createElement('button');
     btnComplete.classList.add('todo-complete');
-    local.btnComplete1 = btnComplete;
+
     ulToDo.appendChild(textLi).appendChild(divBtn).append(btnRemove, btnComplete);
-    local.ulToDo1 = ulToDo.appendChild(textLi).appendChild(divBtn).append(btnRemove, btnComplete);
 
     input.value = '';
     
+    localStorage.setItem("items", ulToDo.innerHTML);
+
     removeToDo(btnRemove);
     completeToDo(btnComplete, textLi);
-    console.log(local);
-
-    localStorage.setItem('items', local);
 };
+
+
 
 const removeToDo = function(element){
     element.addEventListener('click', function(event) {
         element.parentNode.parentNode.remove();
         event.stopPropagation();
+        localStorage.setItem('items', ulToDo.innerHTML);
+        localStorage.setItem("itemsComplete", ulToDoComplete.innerHTML);
     });
 };
 
-const completeToDo = function(element,text){ 
+const completeToDo = function(element, text){ 
     element.addEventListener('click', function() {
         ulToDoComplete.appendChild(text);
+        localStorage.setItem("items", ulToDo.innerHTML);
+        localStorage.setItem("itemsComplete", ulToDoComplete.innerHTML);
+    });
+};
+
+const completeToDoLoad = function(element){ 
+    element.addEventListener('click', function() {
+        ulToDoComplete.appendChild(element)
+        localStorage.setItem("items", ulToDo.innerHTML);
+        localStorage.setItem("itemsComplete", ulToDoComplete.innerHTML);
     });
 };
 
@@ -70,3 +87,57 @@ input.addEventListener("keypress", (keyPressed) => {
     }
 });
 
+function loadToDo(){
+    const data = localStorage.getItem('items');
+    if(data){
+        ulToDo.innerHTML = data;
+    }
+    const btnRemoveLoad = document.querySelectorAll('button.todo-remove');
+    for(const button of btnRemoveLoad){
+        removeToDo(button);
+    }
+    const btnCompleteLoad = document.querySelectorAll('button.todo-complete');
+    const liToDoLoad = document.querySelectorAll('li.todo-item')
+    for(const button of btnCompleteLoad){
+        for(const text of liToDoLoad){
+            completeToDo(button, text);
+        };
+    }   
+};
+function loadToDoComplete(){
+    const data = localStorage.getItem('itemsComplete');
+    if(data){
+        ulToDoComplete.innerHTML = data;
+    }
+    const btnRemoveLoad = document.querySelectorAll('button.todo-remove');
+    for(const button of btnRemoveLoad){
+        removeToDo(button);
+    }     
+};
+
+/* function complateLoadToDo(){
+    const btnCompleteLoad = document.querySelectorAll('button.todo-complete');
+    const liToDoLoad = document.querySelectorAll('li.todo-item')
+    for(const button of btnCompleteLoad){
+        for(const text of liToDoLoad){
+            completeToDo(button, text);
+        };
+    }  
+}; */
+
+loadToDo();
+loadToDoComplete();
+/* complateLoadToDo(); */
+
+
+
+
+/* btnRemoeLoad.addEventListener("click", () => {
+    event.preventDefault();
+    ul.innerHTML = "";
+    localStorage.removeItem('items', ulToDo.innerHTML);
+}); */
+
+//constbtnRemove = document.querySelectorAll('button.todo-remove');
+
+//localStorage.setItem('itemsComplete', ulToDoComplete.innerHTML);
